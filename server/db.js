4073a8640e8f70c2a6eba2712e62d9f75c4f1830
Gayatriallam <gayatriallam02@gmail.com>
2025-09-20 14:21:59 +0530

    Update db.js to handle SSL conditionally for local and Render deployments

--- server/db.js
+++ server/db.js
@@ -1,6 +1,7 @@
 const { Pool } = require("pg");
+const isProduction = process.env.NODE_ENV === "production";
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
+  ...(isProduction && { ssl: { rejectUnauthorized: false } }),
 });
 module.exports = pool;
